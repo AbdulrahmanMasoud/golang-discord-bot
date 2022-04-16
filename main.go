6bf2e2dc1c77e65415cad8fd00a32a83cc18656d
AbdulrahmanMasoud <abdulrahman.masoud.official@gmail.com>
2022-04-16 18:26:22 +0200

    init discord bot

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	conf "github.com/AbdulrahmanMasoud/golang-discord-bot/config"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
@@ -13,9 +12,5 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	c := conf.Config{
-		Token:     os.Getenv("TOKEN"),
-		BotPrefix: os.Getenv("BOT_PREFIX"),
-	}
-	fmt.Print(c.GetToke() + " " + c.GetBotPrefix())
+	fmt.Print(os.Getenv("TOKEN") + " " + os.Getenv("BOT_PREFIX"))
 }
